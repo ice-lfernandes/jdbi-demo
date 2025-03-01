59d0e76f6ce34929d348e80e941976bb920d503b
lfernandes <lfernandes.iteris@alelo.com.br>
2025-03-01 13:24:23 -0300

    fluent api

--- src/main/java/br/com/ldf/medium/jdbi_demo/service/DeveloperService.java
+++ src/main/java/br/com/ldf/medium/jdbi_demo/service/DeveloperService.java
@@ -3,6 +3,7 @@ package br.com.ldf.medium.jdbi_demo.service;
 import br.com.ldf.medium.jdbi_demo.domain.Developer;
 import br.com.ldf.medium.jdbi_demo.persistence.DeveloperRepository;
 import lombok.AllArgsConstructor;
+import org.jdbi.v3.core.Jdbi;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,25 +11,35 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class DeveloperService {
 
-    private final DeveloperRepository developerRepository;
+    private final Jdbi jdbi;
+    private final DeveloperRepository repository;
 
     @Transactional
     public Long saveNewDeveloper(Developer developer) {
-        return developerRepository.saveNewDeveloper(developer);
+        return jdbi.withHandle(handle ->
+                (long) handle.createUpdate("INSERT INTO developer (name) VALUES (:name)")
+                        .bindBean(developer) // bind Bean as parameter
+                        .execute());
     }
 
     @Transactional
     public void updateDeveloper(Developer developer) {
-        developerRepository.updateDeveloper(developer);
+        jdbi.useHandle(handle ->
+                handle.createUpdate("UPDATE developer SET name = :name WHERE id_developer = :id")
+                        .bindBean(developer) // bind Bean as parameter
+                        .execute());
     }
 
     public Developer findDeveloperById(Long developerId) {
-        return developerRepository.findById(developerId)
+        return repository.findById(developerId)
                 .orElseThrow(() -> new RuntimeException("Developer not found"));
     }
 
     public void removeDeveloperById(Long developerId) {
-        developerRepository.removeDeveloperById(developerId);
+        jdbi.useHandle(handle ->
+                handle.createUpdate("DELETE from developer WHERE id_developer = :id")
+                        .bind("id", developerId) // bind primitive as parameter
+                        .execute());
     }
 
 }
